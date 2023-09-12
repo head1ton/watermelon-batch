5e6c24188483450cde83c524e4ba8a0e6f48a851
head1ton <head1ton@gmail.com>
2023-09-13 02:32:30 +0900

    Initial commit

---
+++ src/main/java/ai/watermelonbatch/WatermelonBatchApplication.java
@@ -0,0 +1,13 @@
+package ai.watermelonbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermelonBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermelonBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/watermelonbatch/WatermelonBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.watermelonbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermelonBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
