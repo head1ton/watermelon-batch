815c3cf58be551e1987cd5bf8586d32a1b25151a
head1ton <head1ton@gmail.com>
2023-09-14 02:28:32 +0900

    JobRepository

--- src/main/java/ai/watermelonbatch/sample/jobrepository/JobRunner.java
+++ src/main/java/ai/watermelonbatch/sample/jobrepository/JobRunner.java
@@ -1,5 +1,7 @@
-package ai.watermelonbatch.sample;
+package ai.watermelonbatch.sample.jobrepository;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -9,7 +11,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class JobRunner implements ApplicationRunner {
 
     @Autowired
@@ -20,7 +22,8 @@ public class JobRunner implements ApplicationRunner {
     @Override
     public void run(final ApplicationArguments args) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
-            .addString("name", "user1")
+            .addString("name", "batchJob")
+            .addDate("requestDate", new SimpleDateFormat("yyyy/MM/dd").parse("2020/01/04"))
             .toJobParameters();
 
         jobLauncher.run(job, jobParameters);
