8c4caa3b97ef44568b1190edd7b593738f7aa59a
head1ton <head1ton@gmail.com>
2023-09-15 02:17:21 +0900

    PreventRestart

--- src/main/java/ai/watermelonbatch/sample/job_batch/simplejob/PreventRestartConfiguration.java
+++ src/main/java/ai/watermelonbatch/sample/job_batch/simplejob/PreventRestartConfiguration.java
@@ -1,29 +1,21 @@
 package ai.watermelonbatch.sample.job_batch.simplejob;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class PreventRestartConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -34,9 +26,7 @@ public class ValidatorConfiguration {
                                      .start(step1())
                                      .next(step2())
                                      .next(step3())
-//                                     .validator(new CustomJobParametersValidator())
-                                     .validator(new DefaultJobParametersValidator(
-                                         new String[]{"name", "date"}, new String[]{"count"}))
+                                     .preventRestart()  // 기본값 false
                                      .build();
     }
 
@@ -58,6 +48,7 @@ public class ValidatorConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                                  .tasklet((contribution, chunkContext) -> {
+//                                     throw new RuntimeException("step2 was failed");
                                      System.out.println("step2 has executed");
                                      return RepeatStatus.FINISHED;
                                  })
