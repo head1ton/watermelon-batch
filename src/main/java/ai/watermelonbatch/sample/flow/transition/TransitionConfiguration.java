2fb407246d9b4a9b1f9ac767c714683341feb919
head1ton <head1ton@gmail.com>
2023-09-16 00:50:18 +0900

    Transition

--- src/main/java/ai/watermelonbatch/sample/flow/transition/TransitionConfiguration.java
+++ src/main/java/ai/watermelonbatch/sample/flow/transition/TransitionConfiguration.java
@@ -1,4 +1,4 @@
-package ai.watermelonbatch.sample.flow.batchstatus;
+package ai.watermelonbatch.sample.flow.transition;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.ExitStatus;
@@ -7,8 +7,6 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -16,21 +14,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class BatchStatusExitStatusConfiguration {
+public class TransitionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-
-//    @Bean
-//    @Primary
-//    public Job batchJob() {
-//        return this.jobBuilderFactory.get("batchJob")
-//                                     .start(step1())
-//                                     .next(step2())
-//                                     .build();
-//    }
 
     @Bean
     @Primary
@@ -38,6 +27,12 @@ public class BatchStatusExitStatusConfiguration {
         return this.jobBuilderFactory.get("batchJob")
                                      .start(step1())
                                      .on("FAILED").to(step2())
+                                     .on("FAILED").stop()
+                                     .from(step1())
+                                     .on("*").to(step3())
+                                     .next(step4())
+                                     .from(step2())
+                                     .on("*").to(step5())
                                      .end()
                                      .build();
     }
@@ -51,7 +46,6 @@ public class BatchStatusExitStatusConfiguration {
                                          final StepContribution contribution,
                                          final ChunkContext chunkContext) throws Exception {
 //                                         throw new RuntimeException("step1 was failed");
-                                         contribution.setExitStatus(ExitStatus.FAILED);
                                          return RepeatStatus.FINISHED;
                                      }
                                  })
@@ -73,5 +67,49 @@ public class BatchStatusExitStatusConfiguration {
                                  .build();
     }
 
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(
+                                         final StepContribution contribution,
+                                         final ChunkContext chunkContext) throws Exception {
+//                    throw new RuntimeException("step1 was failed");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .build();
+    }
 
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(
+                                         final StepContribution contribution,
+                                         final ChunkContext chunkContext) throws Exception {
+//                    throw new RuntimeException("step1 was failed");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(
+                                         final StepContribution contribution,
+                                         final ChunkContext chunkContext) throws Exception {
+//                    throw new RuntimeException("step1 was failed");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .build();
+    }
 }
+
