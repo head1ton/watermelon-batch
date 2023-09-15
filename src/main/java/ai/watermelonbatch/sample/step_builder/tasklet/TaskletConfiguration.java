b7302c2f6e74aa1b7064317adc9067fc9c095a6e
head1ton <head1ton@gmail.com>
2023-09-15 14:43:02 +0900

    TasketStep Custom

--- src/main/java/ai/watermelonbatch/sample/step_builder/tasklet/TaskletConfiguration.java
+++ src/main/java/ai/watermelonbatch/sample/step_builder/tasklet/TaskletConfiguration.java
@@ -1,32 +1,21 @@
 package ai.watermelonbatch.sample.step_builder.tasklet;
 
-import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
-import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class TaskletStepConfiguration {
+public class TaskletConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -36,19 +25,21 @@ public class TaskletStepConfiguration {
         return this.jobBuilderFactory.get("batchJob")
                                      .incrementer(new RunIdIncrementer())
 //                                     .start(taskStep())
-                                     .start(chunkStep())
+                                     .start(step1())
+                                     .next(step2())
                                      .build();
     }
 
     @Bean
-    public Step taskStep() {
-        return stepBuilderFactory.get("taskStep")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                                  .tasklet(new Tasklet() {
                                      @Override
                                      public RepeatStatus execute(
                                          final StepContribution contribution,
                                          final ChunkContext chunkContext) throws Exception {
-                                         System.out.println("step was executed");
+                                         System.out.println("stepContribution = " + contribution
+                                             + " chunkContext = " + chunkContext);
                                          return RepeatStatus.FINISHED;
                                      }
                                  })
@@ -56,15 +47,24 @@ public class TaskletStepConfiguration {
     }
 
     @Bean
-    public Step chunkStep() {
-        return stepBuilderFactory.get("chunkStep")
-                                 .<String, String>chunk(10)
-                                 .reader(new ListItemReader<>(
-                                     Arrays.asList("item1", "item2", "item3", "item4", "item5")))
-                                 .processor(
-                                     (ItemProcessor<String, String>) String::toUpperCase)
-                                 .writer(items -> items.forEach(System.out::println))
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(
+                                         final StepContribution contribution,
+                                         final ChunkContext chunkContext) throws Exception {
+
+                                         System.out.println("stepContribution = " + contribution
+                                             + " chunkContext = " + chunkContext);
+
+                                         throw new RuntimeException("step2 was failed");
+//                                         return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .startLimit(3)
                                  .build();
     }
+
 
 }
