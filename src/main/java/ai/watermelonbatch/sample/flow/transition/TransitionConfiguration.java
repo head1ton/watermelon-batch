fc7084f8f2487a11021f2c7c604976c0f31ce4ae
head1ton <head1ton@gmail.com>
2023-09-16 01:56:55 +0900

    CustomExitStatusConfiguration

--- src/main/java/ai/watermelonbatch/sample/flow/transition/TransitionConfiguration.java
+++ src/main/java/ai/watermelonbatch/sample/flow/transition/TransitionConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class TransitionConfiguration {
 
