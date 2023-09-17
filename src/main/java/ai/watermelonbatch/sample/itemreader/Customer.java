ac46135563f6ef2db86b9c69afee73d0b2e4c3b9
head1ton <head1ton@gmail.com>
2023-09-18 02:06:00 +0900

    JsonConfiguration

---
+++ src/main/java/ai/watermelonbatch/sample/itemreader/Customer.java
@@ -0,0 +1,14 @@
+package ai.watermelonbatch.sample.itemreader;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private String id;
+    private String name;
+    private int age;
+
+}
