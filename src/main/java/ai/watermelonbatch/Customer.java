0e755e53b4a54a62659884fbbbd1a2b2648caac1
head1ton <head1ton@gmail.com>
2023-09-18 11:54:04 +0900

    JdbcPagingItemReaderConfiguration

---
+++ src/main/java/ai/watermelonbatch/Customer.java
@@ -0,0 +1,23 @@
+package ai.watermelonbatch;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
